b5de8e59d799693d8078d81200ceb7edc4b491d6
LinsPaulson <158697383+LinsPaulson@users.noreply.github.com>
2025-09-11 18:43:22 +0400

    Update wifi_config.h

--- VScode/ESP32_DHT11_ThingsBoard_Optimized/main/wifi_config.h
+++ VScode/ESP32_DHT11_ThingsBoard_Optimized/main/wifi_config.h
@@ -1,10 +1,10 @@
 #ifndef WIFI_CONFIG_H
 #define WIFI_CONFIG_H
 
-#define WIFI_SSID "TP-Link_FA44"
-#define WIFI_PASS "09506428"
+#define WIFI_SSID "Your SSID"
+#define WIFI_PASS "Your password"
 
 #define THINGSBOARD_SERVER "demo.thingsboard.io" // e.g., "demo.thingsboard.io"
-#define THINGSBOARD_TOKEN "ni51whcqmejaceg6lyek"
+#define THINGSBOARD_TOKEN "your token"
 
 #endif
